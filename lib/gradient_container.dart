5f07756627457cff3fa60723de25c80157c4d59a
agakoz <agataakoziol@gmail.com>
2023-07-04 14:23:40 +0800

    dice rolling app

---
+++ lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : colors = const [
+          Color.fromARGB(255, 62, 32, 146),
+          Color.fromARGB(255, 28, 3, 70)
+        ];
+
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'gradient_container.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Colors.deepPurple,
+        body: GradientContainer.purple(),
+      ),
+    ),
+  );
+}
